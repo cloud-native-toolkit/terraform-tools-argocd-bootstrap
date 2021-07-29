23b5b34d791d1853965e56751269aada55fbca7a
Sean Sundberg <seansund@us.ibm.com>
2021-07-29 01:17:15 -0500

    Adds artifactory and sonarqube to test (#9)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ test/stages/stage1-artifactory.tf
@@ -0,0 +1,10 @@
+module "gitops_artifactory" {
+  source = "github.com/cloud-native-toolkit/terraform-gitops-artifactory"
+
+  gitops_config = module.gitops.gitops_config
+  git_credentials = module.gitops.git_credentials
+  namespace = module.gitops_namespace.name
+  cluster_ingress_hostname = module.dev_cluster.platform.ingress
+  cluster_type             = module.dev_cluster.platform.type_code
+  tls_secret_name          = module.dev_cluster.platform.tls_secret
+}
---
+++ test/stages/stage1-sonarqube.tf
@@ -0,0 +1,11 @@
+module "gitops_sonarqube" {
+  source = "github.com/cloud-native-toolkit/terraform-gitops-sonarqube"
+
+  gitops_config            = module.gitops.gitops_config
+  git_credentials          = module.gitops.git_credentials
+  namespace                = module.gitops_namespace.name
+  cluster_ingress_hostname = module.dev_cluster.platform.ingress
+  cluster_type             = module.dev_cluster.platform.type_code
+  tls_secret_name          = module.dev_cluster.platform.tls_secret
+  kubeseal_cert            = module.argocd-bootstrap.sealed_secrets_cert
+}
