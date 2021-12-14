83fee14599999fc96b8d604b60e6ddb306ecb2fe
Sean Sundberg <seansund@us.ibm.com>
2021-12-14 15:11:06 -0600

    Bumps openshift-cicd to v1.7.6 (#2)

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "openshift_cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.5"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.6"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
