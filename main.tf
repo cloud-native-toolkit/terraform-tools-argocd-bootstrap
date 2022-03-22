ae4dca176beb71b7706dd2f2d61410d85e0793ce
Sean Sundberg <seansund@us.ibm.com>
2022-03-22 00:30:45 -0500

    Updates submodule versions (#9)
    * Updates submodule versions
    
        - openshift-cicd bumped to v1.7.10
        - gitops-bootstrap bumped to v1.2.9
    
    Closes #8
    
    * Updates workflows
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
 
 module "openshift_cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.8"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.10"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
   cluster_config_file = var.cluster_config_file
   olm_namespace       = var.olm_namespace
   operator_namespace  = var.operator_namespace
-  app_namespace       = "default"
   sealed_secret_cert  = var.sealed_secret_cert
   sealed_secret_private_key = var.sealed_secret_private_key
 }
 
 module "bootstrap" {
-  source = "github.com/cloud-native-toolkit/terraform-util-gitops-bootstrap.git?ref=v1.2.2"
+  source = "github.com/cloud-native-toolkit/terraform-util-gitops-bootstrap.git?ref=v1.2.9"
 
   cluster_config_file = var.cluster_config_file
   gitops_namespace    = module.openshift_cicd.argocd_namespace
