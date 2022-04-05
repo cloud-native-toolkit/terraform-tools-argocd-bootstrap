e5154f4fb9d72f8be5e939a9b6059f355924e47f
Sean Sundberg <seansund@us.ibm.com>
2022-04-05 15:41:08 -0500

    Updates openshift-cicd module to v1.7.13
    closes #14
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "openshift_cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.12"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.13"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
