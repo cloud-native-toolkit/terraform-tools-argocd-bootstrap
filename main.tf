416ffa4d9c2d08cb8dc1065914cb0f588b620de2
Sean Sundberg <seansund@us.ibm.com>
2022-04-14 11:15:53 -0500

    Updates openshift-cicd submodule to v1.7.15
    - Fixes timing issue in argocd with openshift-gitops namespace
    
    closes #23
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "openshift_cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.14"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.15"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
