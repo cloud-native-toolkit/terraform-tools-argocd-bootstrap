67cafa678672fa422d8fb35a60f17e5557f10718
Andrew Trice <amtrice@us.ibm.com>
2022-04-27 10:13:04 -0400

    updated openshift-cicd to v1.7.18 (#29)
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "openshift_cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.17"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.18"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
