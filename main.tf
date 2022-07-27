7e18f15990e5639b62d17d103308106825410db3
cloudnativetoolkit <cloudnativetoolkit@users.noreply.github.com>
2022-07-27 14:01:26 +0000

    Updates cloud-native-toolkit/terraform-tools-openshift-cicd module to v1.11.1
    Signed-off-by: GitHub <noreply@github.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "openshift_cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.11.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.11.1"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
