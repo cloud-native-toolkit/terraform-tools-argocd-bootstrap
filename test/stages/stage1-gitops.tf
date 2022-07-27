da5f35cc8a96f26a55704a4c4c5ecd0f45877640
Sean Sundberg <seansund@us.ibm.com>
2022-07-27 13:23:55 -0500

    Adds gitea support to test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-gitops.tf
+++ test/stages/stage1-gitops.tf
@@ -1,13 +1,35 @@
+
+module "gitea" {
+  source = "github.com/cloud-native-toolkit/terraform-tools-gitea"
+
+  cluster_config_file = module.dev_cluster.config_file_path
+  cluster_type = module.dev_cluster.platform.type_code
+  instance_name = var.gitea_instance_name
+  instance_namespace = module.gitea_namespace.name
+  olm_namespace = module.dev_software_olm.olm_namespace
+  operator_namespace = module.dev_software_olm.target_namespace
+  password = var.gitea_password
+  username = var.gitea_username
+}
+module "gitea_namespace" {
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace"
+
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  create_operator_group = true
+  name = var.namespace
+}
 module "gitops" {
   source = "github.com/cloud-native-toolkit/terraform-tools-gitops"
 
-  host = var.git_host
-  type = var.git_type
-  org  = var.git_org
+  debug = false
+  gitea_host = module.gitea.host
+  gitea_org = module.gitea.org
+  gitea_token = module.gitea.token
+  gitea_username = module.gitea.username
   repo = var.git_repo
-  token = var.git_token
-  username = var.git_username
   gitops_namespace = var.gitops_namespace
+  sealed_secrets_cert = module.cert.cert
+  strict = true
 }
 
 resource null_resource gitops_output {
