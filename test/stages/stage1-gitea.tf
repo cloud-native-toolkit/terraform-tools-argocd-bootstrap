9b3d9866a98d6102c77d9af5cb6000925de6063f
Sean Sundberg <seansund@us.ibm.com>
2022-10-05 23:45:09 -0500

    Configures test for gitea
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ test/stages/stage1-gitea.tf
@@ -0,0 +1,9 @@
+module "gitea" {
+  source = "github.com/cloud-native-toolkit/terraform-tools-gitea"
+
+  cluster_config_file = module.dev_cluster.config_file_path
+  olm_namespace       = module.dev_software_olm.olm_namespace
+  operator_namespace  = module.dev_software_olm.target_namespace
+  instance_namespace  = module.gitops_namespace.name
+  ca_cert             = module.dev_cluster.ca_cert
+}
--- test/stages/stage1-gitops.tf
+++ test/stages/stage1-gitops.tf
@@ -1,13 +1,22 @@
 module "gitops" {
   source = "github.com/cloud-native-toolkit/terraform-tools-gitops"
 
-  host = var.git_host
-  type = var.git_type
-  org  = var.git_org
+  host = ""
+  org  = ""
   repo = var.git_repo
-  token = var.git_token
   username = var.git_username
+  token = var.git_token
+  project = ""
   gitops_namespace = var.gitops_namespace
+  sealed_secrets_cert = module.cert.cert
+  strict = true
+  gitea_host = module.gitea.host
+  gitea_org = module.gitea.org
+  gitea_username = module.gitea.username
+  gitea_token = module.gitea.token
+  gitea_ca_cert = module.gitea.ca_cert
+  ca_cert_file = ""
+  debug = true
 }
 
 resource null_resource gitops_output {
