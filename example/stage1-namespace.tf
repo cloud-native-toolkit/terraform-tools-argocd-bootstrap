e1ecd690159af0c7df2f8654ffe6da3400bcb79f
Sean Sundberg <seansund@us.ibm.com>
2023-06-06 21:46:33 -0500

    Add namespace module
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ example/stage1-namespace.tf
@@ -0,0 +1,7 @@
+
+module "dev_tools_namespace" {
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git"
+
+  cluster_config_file_path = module.cluster.config_file_path
+  name                     = var.namespace
+}
