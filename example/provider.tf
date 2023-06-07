7d056db969f74a79bb11ba2e4c6b74377e0e57a8
Sean Sundberg <seansund@us.ibm.com>
2023-06-07 09:03:27 -0500

    Update gitops config with gitea outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- example/provider.tf
+++ example/provider.tf
@@ -1,5 +1,10 @@
 provider "gitops" {
-  bin_dir  = data.clis_check.test_clis.bin_dir
+  bin_dir          = data.clis_check.test_clis.bin_dir
+  default_host     = module.gitea.host
+  default_org      = module.gitea.org
+  default_username = module.gitea.username
+  default_token    = module.gitea.token
+  default_ca_cert  = module.gitea.ca_cert
 }
 
 provider "ibm" {
