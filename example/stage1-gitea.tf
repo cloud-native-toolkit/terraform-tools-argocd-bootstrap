d2f94b20846ff7e65f79a0e9be3f526ad90b3a83
Cloud-Native Toolkit <79655631+cloudnativetoolkit@users.noreply.github.com>
2023-07-10 12:57:11 -0500

    Updates cloud-native-toolkit/terraform-tools-openshift-cicd module to v2.0.1 (#56)
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>
    Co-authored-by: cloudnativetoolkit <cloudnativetoolkit@users.noreply.github.com>
    Co-authored-by: Sean Sundberg <seansund@us.ibm.com>

--- example/stage1-gitea.tf
+++ example/stage1-gitea.tf
@@ -1,9 +1,16 @@
+module "gitea_namespace" {
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git"
+
+  cluster_config_file_path = module.cluster.config_file_path
+  name                     = "gitea"
+}
+
 module "gitea" {
   source = "github.com/cloud-native-toolkit/terraform-tools-gitea"
 
   cluster_config_file = module.cluster.config_file_path
   ingress_subdomain   = module.cluster.platform.ingress
   tls_secret_name     = module.cluster.platform.tls_secret
-  instance_namespace  = module.dev_tools_namespace.name
+  instance_namespace  = module.gitea_namespace.name
   preserve_volumes    = false
 }
--- example/stage1-namespace.tf
+++
@@ -1,7 +0,0 @@
-
-module "dev_tools_namespace" {
-  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git"
-
-  cluster_config_file_path = module.cluster.config_file_path
-  name                     = var.namespace
-}
